6709ec76e7d3ad834623c2311f084af8c97c0115
nori <nori901117@gmail.com>
2024-06-19 20:44:12 +0900

    remove user avatar icons

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -42,8 +42,8 @@ const MeetingCard = ({
         </div>
       </article>
       <article className={cn("flex justify-center relative", {})}>
-        <div className="relative flex w-full max-sm:hidden">
-          {avatarImages.map((img, index) => (
+        {/* <div className="relative flex w-full max-sm:hidden"> */}
+        {/* {avatarImages.map((img, index) => (
             <Image
               key={index}
               src={img}
@@ -53,11 +53,11 @@ const MeetingCard = ({
               className={cn("rounded-full", { absolute: index > 0 })}
               style={{ top: 0, left: index * 28 }}
             />
-          ))}
-          <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
+          ))} */}
+        {/* <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
             +5
-          </div>
-        </div>
+          </div> */}
+        {/* </div> */}
         {!isPreviousMeeting && (
           <div className="flex gap-2">
             <Button onClick={handleClick} className="rounded bg-blue-1 px-6">
